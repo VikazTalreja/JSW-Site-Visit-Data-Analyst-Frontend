ea1713b0678d2e5ffcb4857d946d77e7cdba1068
Vikas Talreja <vikazztalreja@gmail.com>
2025-03-28 01:45:14 +0530

    login Update

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
       id: i,
       left: `${Math.random() * 100}%`,
       size: 2 + Math.random() * 5,
-      delay: Math.random() * 8,
+      delay: 10+ Math.random() * 8,
       duration: 1 + Math.random() * 15,
       opacity: 0.3 + Math.random() * 0.3
     }));
@@ -47,39 +47,39 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-6 bg-gray-900 text-white relative">
-      <div className="bg-gray-800 shadow-lg rounded-lg p-8 w-full max-w-sm relative z-10">
-        <h1 className="text-3xl font-bold text-center">JSW Steel</h1>
-        <p className="text-sm text-gray-400 text-center mt-2">Sales Insights Dashboard</p>
+    <main className="flex min-h-screen flex-col items-center justify-center p-6 bg-gray-200 text-white relative">
+      <div className="bg-white shadow-lg shadow-blue-200 rounded-2xl p-8 w-full max-w-sm relative z-10">
+        <h1 className="text-3xl text-black font-bold text-center">JSW Steel</h1>
+        <p className="text-sm text-gray-700 text-center mt-2">Sales Insights Dashboard</p>
         
         {error && <div className="mt-4 p-2 text-sm text-red-500 text-center">{error}</div>}
         
         <form onSubmit={handleLogin} className="mt-6">
           <div className="mb-4">
-            <label className="block text-gray-300">Email Address</label>
+            <label className="block text-gray-950">Email Address</label>
             <input
               type="email"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="w-full px-3 py-2 bg-gray-700 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+              className="w-full px-3 py-2 text-black  border border-gray-800 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
               placeholder="Enter your email"
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-300">Password</label>
+            <label className="block text-black">Password</label>
             <input
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-2 bg-gray-700 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+              className="w-full px-3 py-2 text-black border border-gray-800 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
               placeholder="Enter your password"
               required
             />
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-500 hover:bg-blue-600 transition py-2 rounded-md font-bold"
+            className="w-full bg-black hover:cursor-pointer transition py-2 rounded-md font-bold"
           >
             Sign In
           </button>
@@ -87,11 +87,11 @@ export default function Home() {
       </div>
 
       {/* Pixel Animation */}
-      <div className="absolute inset-0 overflow-hidden">
+      {/* <div className="absolute inset-72 overflow-hidden">
         {pixels.map((pixel) => (
           <div
             key={pixel.id}
-            className="absolute bg-blue-400 rounded-full opacity-50"
+            className="absolute  bg-blue-400 rounded-full opacity-50"
             style={{
               left: pixel.left,
               width: `${pixel.size}px`,
@@ -101,12 +101,12 @@ export default function Home() {
             }}
           />
         ))}
-      </div>
+      </div> */}
 
       <style jsx>{`
         @keyframes float {
           0% { transform: translateY(0); opacity: 0.5; }
-          50% { transform: translateY(-20px); opacity: 1; }
+          50% { transform: translateY(100px); opacity: 1; }
           100% { transform: translateY(0); opacity: 0.5; }
         }
       `}</style>
